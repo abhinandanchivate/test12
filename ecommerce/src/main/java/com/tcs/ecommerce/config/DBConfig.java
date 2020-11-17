d14c9c0088b2276311db9e435d2c05aaf598795f
abhinandan chivate <abhinandanchivate2000@gmail.com>
2020-11-17 19:26:31 +0000

    working code

--- ecommerce/src/main/java/com/tcs/ecommerce/config/DBConfig.java
+++ ecommerce/src/main/java/com/tcs/ecommerce/config/DBConfig.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -53,12 +55,19 @@ public class DBConfig {
 		properties.setProperty("hibernate.format_sql", environment.getProperty("hibernate.format_sql"));
 		properties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
 		properties.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
-		
+		entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
 		entityManagerFactoryBean.setPackagesToScan("com.tcs.ecommerce.model");
 		entityManagerFactoryBean.setJpaProperties(properties);
 		
 		return entityManagerFactoryBean;
 		
+	}
+	
+	@Bean
+	public JpaTransactionManager transactionManager() {
+		JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+		jpaTransactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+		return jpaTransactionManager;
 	}
 
 }
