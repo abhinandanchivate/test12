6c3bfb8f579ab5b58c7e4f74c806e54125854447
abhinandan chivate <abhinandanchivate2000@gmail.com>
2020-11-18 15:54:15 +0000

    few service methods added

--- ecommerce/src/main/java/com/tcs/ecommerce/service/ProductService.java
+++ ecommerce/src/main/java/com/tcs/ecommerce/service/ProductService.java
@@ -4,9 +4,13 @@ import java.util.Optional;
 
 import com.tcs.ecommerce.model.Product;
 
+import antlr.collections.List;
+
 public interface ProductService {
 	
 	public String createProduct(Product product);
 	public Optional<Product> getProductById(int id);
+	public void deleteProduct(int id);
+	public Optional<java.util.List<Product>> getProducts();
 
 }
