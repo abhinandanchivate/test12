b8566b771428399b4bcdf9861a1682534a97fd77
abhinandan chivate <abhinandanchivate2000@gmail.com>
2020-11-19 19:35:35 +0000

    controller and config

---
+++ mvcdemo/src/main/java/com/tcs/mvcdemo/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.tcs.mvcdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.tcs.mvcdemo")
+public class WebConfig {
+
+}
---
+++ mvcdemo/src/main/java/com/tcs/mvcdemo/controller/HomeController.java
@@ -0,0 +1,10 @@
+package com.tcs.mvcdemo.controller;
+
+import javax.swing.SwingContainer;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+}
