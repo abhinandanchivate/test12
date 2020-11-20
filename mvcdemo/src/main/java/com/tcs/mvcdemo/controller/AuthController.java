65b10eb759bedc3aa095c66d1387a913f24457da
abhinandan chivate <abhinandanchivate2000@gmail.com>
2020-11-20 18:51:55 +0000

    repo in controlle

--- mvcdemo/src/main/java/com/tcs/mvcdemo/controller/AuthController.java
+++ mvcdemo/src/main/java/com/tcs/mvcdemo/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.tcs.mvcdemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -7,11 +8,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tcs.mvcdemo.model.Login;
+import com.tcs.mvcdemo.repository.LoginRepository;
 
 @Controller
 @RequestMapping(path="/auth")
 public class AuthController {
 
+	@Autowired
+	LoginRepository loginRepository;
 	@GetMapping("/login.html")
 	public String getLoginPage() {
 		return "login";
@@ -20,6 +24,13 @@ public class AuthController {
 	@PostMapping("/login.html")
 	public String validateLogin(@ModelAttribute Login login) {
 		System.out.println("hello from validatelogin"+login);
+		
+		if(login.equals(loginRepository.findById(login.getUserName()).get())) {
+			System.out.println("success");
+		}
+		else {
+			System.out.println("fail");
+		}
 		return "home";
 	}
 }
--- mvcdemo/src/main/java/com/tcs/mvcdemo/model/Login.java
+++ mvcdemo/src/main/java/com/tcs/mvcdemo/model/Login.java
@@ -1,14 +1,21 @@
 package com.tcs.mvcdemo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+@Entity
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Login {
-	
+	@Id
+	@Column(length =20 )
 	private String userName;
 	private String password;
 
