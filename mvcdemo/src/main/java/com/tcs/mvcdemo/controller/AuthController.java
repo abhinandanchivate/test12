2893b73607c61afa5e28a1412bd5b0fb19d79949
abhinandan chivate <abhinandanchivate2000@gmail.com>
2020-11-20 17:21:23 +0000

    login post

--- mvcdemo/src/main/java/com/tcs/mvcdemo/controller/AuthController.java
+++ mvcdemo/src/main/java/com/tcs/mvcdemo/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.tcs.mvcdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +12,11 @@ public class AuthController {
 	@GetMapping("/login.html")
 	public String getLoginPage() {
 		return "login";
+	}
+	
+	@PostMapping("/login.html")
+	public String validateLogin() {
+		System.out.println("hello from validatelogin");
+		return "home";
 	}
 }
--- mvcdemo/src/main/webapp/WEB-INF/views/login.jsp
+++ mvcdemo/src/main/webapp/WEB-INF/views/login.jsp
@@ -8,5 +8,21 @@
 </head>
 <body>
 THis is Login Page
+
+<table>
+<form action="/auth/login.html" method="post">
+<tr>
+<td>UserName
+</td>
+<td><input type="text" name="userName" />
+</tr>
+<tr>
+<td>Password
+</td>
+<td><input type="text" name="password" />
+</tr><tr>
+<td><input type="submit"/></td></tr>
+</form>
+</table>
 </body>
 </html>
