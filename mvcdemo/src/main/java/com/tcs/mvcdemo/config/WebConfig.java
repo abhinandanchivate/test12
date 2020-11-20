16bf424cc126abbdf5681feb8ab268bb551767fd
abhinandan chivate <abhinandanchivate2000@gmail.com>
2020-11-20 16:05:31 +0000

    DS Config

--- mvcdemo/src/main/java/com/tcs/mvcdemo/config/WebConfig.java
+++ mvcdemo/src/main/java/com/tcs/mvcdemo/config/WebConfig.java
@@ -2,8 +2,10 @@ package com.tcs.mvcdemo.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc // it will enable the mvc env (@requestbaody, controller , response body, requestmapping, @valid
 @ComponentScan("com.tcs.mvcdemo")
 public class WebConfig {
 
--- mvcdemo/src/main/webapp/WEB-INF/views/home.jsp
+++ mvcdemo/src/main/webapp/WEB-INF/views/home.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+Hello From Spring MVC
 </body>
 </html>
